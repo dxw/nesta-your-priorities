3f29a18f6ec4fbc542ebc31fc288266d14c09265
Ell Brownlee <ell.brownlee@dxw.com>
2026-07-17 15:53:04 +0100

    Add plugins to test runner

--- webApps/client/web-test-runner.config.mjs
+++ webApps/client/web-test-runner.config.mjs
@@ -6,14 +6,23 @@ import rollupCommonjs from '@rollup/plugin-commonjs';
 const commonjs = fromRollup(rollupCommonjs);
 
 export default {
-  nodeResolve: true,
+  nodeResolve: {
+    browser: true,
+  },
   preserveSymlinks: true,
   files: 'src/**/*.test.ts',
   plugins: [
     commonjs({
       include: [
+        '**/node_modules/debug/**',
+        '**/node_modules/glob-to-regexp/**',
+        '**/node_modules/is-subset/**',
         '**/node_modules/i18next-http-backend/**',
         '**/node_modules/fetch-mock/**',
+        '**/node_modules/lodash.isequal/**',
+        '**/node_modules/ms/**',
+        '**/node_modules/path-to-regexp/**',
+        '**/node_modules/querystring/**',
       ],
     }),
     esbuildPlugin({
