dcdc55d81becbba8f4bf700a9b5b45e0cf7712fb
Ell Brownlee <ell.brownlee@dxw.com>
2026-09-07 17:54:08 +0100

    Update submission form idea criteria copy

--- webApps/client/src/yp-post/yp-post-edit.ts
+++ webApps/client/src/yp-post/yp-post-edit.ts
@@ -1522,17 +1522,17 @@ export class YpPostEdit extends YpEditBase {
 <ul>
 <li><b>Practical</b> - a solution, not just a complaint</li>
 <li><b>Do-able</b> - it must be something that could realistically change</li>
-<li><b>Relatively inexpensive</b> - if it requires too much money they won’t do it</li>
+<li><b>Relatively inexpensive</b> - if it costs too much, government, regulators, or councils won't do it</li>
 <li><b>A widespread issue</b> - whether it could help millions of people a little, or thousands of people a lot</li>
-<li>And crucially… <b>hard to disagree with</b> - the kind of thing that 80% of people, whatever their politics, would say “yeah, that makes sense”.</li>
+<li>And crucially… <b>hard to disagree with</b> - the kind of thing that 80% of people, whatever their politics, would say “yeah, that makes sense”</li>
 </ul>
 
 <h3>What we’ll say no to:</h3>
 <ul>
 <li><b>Too local</b> - a drinking water fountain in your local park won't cut it</li>
 <li><b>Overly political</b> - no party lines, and no dog whistles</li>
-<li><b>Illegal</b> - breaks the law, criminal or otherwise.</li>
-<li><b>Harmful</b> - nothing that harms people or animals.</li>
+<li><b>Illegal</b> - breaks the law, criminal or otherwise</li>
+<li><b>Harmful</b> - nothing that harms people or animals</li>
 </ul>
 <p>&nbsp;</p>
       </div>
